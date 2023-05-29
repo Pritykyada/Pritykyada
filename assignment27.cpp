46939f3dc878297b17d7fb8e2301f1158d8f8dc5
pritee <pritikyada22@gmail.com>
2023-05-29 19:14:58 +0530

    29/5/2023

--- assignment27.cpp
+++ assignment27.cpp
@@ -1,3 +1,104 @@
+//overload c3=-C1+C2;
+/*#include<iostream>
+using namespace std;
+class student
+{
+    int a,b;
+    public :
+        friend ostream& operator<<(ostream &,student);
+        friend istream& operator>>(istream&,student &);
+        student(){  cout<<endl<<"default constructor call"<<endl;}
+        student(int s,int n){
+            cout<<endl<<"peramiterized constructor call"<<endl;
+            a=s;
+            b=n;
+        }
+        
+       student operator-()
+       {
+            cout<<"-operator overload"<<endl;
+            student temp;
+            temp.a=-a;
+            temp.b=-b;
+            return temp;
+       }
+       student (const student &c)
+        {
+            cout<<endl<<"copy constructor call"<<endl;
+            a=c.a;
+            b=c.b;
+        }
+       student operator+(student s)
+       {
+        
+            student temp;
+            cout<<"+ operator call"<<endl;
+            temp.a=a+s.a;
+            temp.b=b+s.b;
+            return temp;
+       }
+       void showdata()
+       {
+        cout<<"a-->"<<a<<"b-->"<<b<<endl;
+       }
+      student operator++(int)
+      {
+        cout<<"++operator called";
+            student temp;
+            temp.a=a++;
+           temp. b=b++;
+           return temp;
+      }
+      student operator++()
+      {
+
+        student temp;
+        temp.a=++a;
+        temp.b=++b;
+        return temp;
+      }
+};
+ostream & operator<<(ostream &out,student data)
+{
+    static int a=0;
+    cout<<"<<operator called"<<endl;
+    cout<<a++;
+    out<<"a-->"<<data.a;
+    out<<"b-->"<<data.b;
+    return out;
+}
+istream & operator>>(istream &in,student &data)
+{   
+        cout<<"enter a:";
+        in>>data.a;
+        cout<<endl<<"enter b:";
+        in>>data.b;
+        return in;
+
+}
+int main()
+{
+    student c1(6,3),c4,c5;
+    cout<<" c1 data:";
+    c1.showdata();
+    student c2=c1;
+    cout<<" c2 data:";
+    c2.showdata();
+    cout<<endl<<" after addition  data:";
+   // student c3=c5+c4;
+    student c3=-c1+c2;
+    c3.showdata();
+    cout<<endl<<" pre incriment  data:";
+    (++c3).showdata();
+    cout<<endl<<" post incriment  data:"<<endl;
+    cout<<c3<<(c3++);
+    cout<<endl;
+    (c3++).showdata();
+    c3.showdata();
+    cout<<"take input from user";
+    cin>>c4>>c5;
+    cout<<c4<<c5;
+}*/
 
 /*Assignment - 27 A Job Ready Bootcamp in C++, DSA and IOT MySirG
 Operator overloading and friend function
@@ -179,8 +280,9 @@ int main()
 Write a C++ program using operator overloading for the following:
 1. = = : To check whether two Times are the same or not.
 2. >> : To accept the time.
-3. << : To display the time*/
-#include <iostream>
+3. << : To display the time
+4.<    : oveload operator*/
+/*#include <iostream>
 using namespace std;
 class time
 {
@@ -190,23 +292,30 @@ public:
     int second;
 
 public:   
-    void operator==(time y)
+    int operator==(time y)
         {
-        cout<<"==============================="<<endl;
-        if(hour==y.hour && second==y.second  && minute==y.minute)
-        {
-            cout<<"times are equal";
+            cout<<"==============================="<<endl;
+            if(hour==y.hour)
+            {
+                if(minute==y.minute)
+                {
+                    if(second==y.second)
+                    {
+                        return 1;
+                    }
+                }
+            }
+            return 0;
         }
-        else
-        {
-            cout<<"times are not equal";
-        }
-        }
-    friend void operator>>(istream &,time &);
-    friend void operator<<(ostream &,time &);
+    bool operator>(time &data);
+    friend istream & operator>>(istream &,time &);
+    friend ostream & operator<<(ostream &,time &);
+
+   
 };
 
-void operator>>(istream &cin,time &t)
+
+istream & operator>>(istream &cin,time &t)
 {
     cout<<"enter hour:";
     cin>>t.hour;
@@ -214,11 +323,43 @@ void operator>>(istream &cin,time &t)
     cin>>t.minute;
     cout<<"enter second:";
     cin>>t.second;
+    return cin;
 }
-void operator<<(ostream &cout,time &p)
+ostream & operator<<(ostream &cou,time &p)
 {
-    cout<<"hour:"<<p.hour<<endl<<"minute:"<<p.minute<<endl<<"second"<<p.second<<endl;
+    cou<<p.hour<<":"<<p.minute<<":"<<p.second<<endl;
+    return cou;
+
 }
+bool time::operator>(time &data)
+    {
+       if(hour>data.hour)
+       {
+            return 1;
+       }
+       else
+       {
+            if(hour==data.hour)
+            {
+                if(minute>data.minute)
+                {
+                    return 1;
+                }
+                else
+                {
+                    if(minute==data.minute)
+                    {
+                            if(second>data.second)
+                            {
+                                return 1;
+                            }
+                    }
+                }
+            }
+             return 0;
+       }
+
+    }
 int main()
 {
     time t1, t2;
@@ -233,6 +374,386 @@ int main()
     cout<<t1;
     cout<<" second time:";
     cout<<t2;
-    t1==t2;
+   if(t1==t2)
+   {
+    cout<<"time t1 and t2 is equal"<<endl;
+   }
+   else
+   {
+    cout<<"time t1 and t2 is not equal"<<endl;
+   }
+   if(t1>t2)
+   {
+        cout<<"time t1 is grater"<<endl;
+   }
+   else
+   {
+         cout<<"time t2 is grater"<<endl;
+   }
+}*/
+/*5. Consider following class Numbers
+class Numbers
+{
+int x,y,z;
+public:
+// methods
+};
+Overload the operator unary minus (-) to negate the numbers.*/
+/*#include<iostream>
+using namespace std;
+class number
+{
+    private:
+        int x,y,z;
+    public:
+    void setdata(int a,int b,int c)
+    {
+        x=a;
+        y=b;
+        z=c;
+    }
+    void showdata()
+    {
+        cout<<"x==>"<<x<<"y==>"<<y<<"==>"<<z;
+    }
+    number oparator-()
+    {
+        number u;
+        u.x=-x;
+        u.y=-y;
+        u.z=-z;
+        return u;
+    }
+};
+int main()
+{
+    number n,n1;
+    n.setdata(3,6,7);
+    n1=-n;
+    n.showdata();
+}*/
+
+/*6. Create a class CString to represent a string.
+a) Overload the + operator to concatenate two strings.
+b) == to compare 2 strings.*/
+/*#include<string.h>
+#include<iostream>
+using namespace std;
+class str
+{
+    private:
+        char name[18];
+    public:
+        void setdata(const char *a)
+        {
+           strcpy(name,a);
+        } 
+       str operator+(str b)
+       {
+            str temp;
+            strcpy(temp.name,name);
+            strcat(temp.name,b.name);
+            return temp;
+       }
+       void showdata()
+       {
+            cout<<"name==>"<<name;
+       }
+       void operator==(str n)
+       {
+            if(strcmp(name,n.name))
+            {
+                cout<<"both strings not equal";
+            }
+            else
+            {
+                cout<<"both strings equal";
+            }
+       }
+};
+int main()
+{
+    str s1,s2,s3;
+    char str[]="prty";
+    char str1[]="prty";
+    s1.setdata("prity");
+    s2.setdata(str1);
+    s3=s1+s2;
+    cout<<"s1==>:";
+    s1.showdata();
+    cout<<endl<<"s2==>:";
+    s2.showdata();
+    cout<<endl<<"concate string:";
+    s3.showdata();
+    s1==s2;
+}*/
+/*7. Define a C++ class fraction
+class fraction
+{
+long numerator;
+long denominator;
+Public:
+fraction (long n=0, long d=0);
+}
+Overload the following operators as member or friend:
+a) Unary ++ (pre and post both)
+b) Overload as friend functions: operators << and >>.*/
+/*#include<iostream>
+using namespace std;
+class fraction
+{
+    private:
+    long numerator;
+    long denominator;
+    public:
+    fraction (long n=0,long d=0)
+    {
+        numerator=n;
+        denominator=d;  
+    }
+    fraction  operator++()
+    {
+        fraction temp;
+        temp.numerator=++numerator;  
+        temp.denominator=++denominator;
+        return temp;
+    }
+    friend  fraction operator++(fraction &,int);
+    friend void operator>>(istream &,fraction &);
+    friend void operator<<(ostream &,fraction &);
+
+};
+void operator<<(ostream &out,fraction &data)
+{
+    out<<data.numerator<<"/"<<data.denominator<<endl;
 
 }
+void operator>>(istream &c,fraction &a)
+{
+    cout<<endl<<"numerator:";
+    c>>a.numerator;
+    cout<<endl<<"denominator:";
+    c>>a.denominator;
+}
+fraction operator++(fraction &s ,int)
+{ 
+    fraction temp;
+    temp.numerator=s.numerator++;
+    temp.denominator=s.denominator++;
+    return temp;
+}
+int main()
+{
+    fraction f,f2;
+    cout<<"f    :"<<f;
+     cout<<"f2   :"<<f2;
+     cout<<endl<<"enter 1 fraction number"<<endl;
+     cin>>f;
+     cout<<endl<<"f++   :";
+     f++;
+     cout<<f;
+    cout<<"++f   :";
+     ++f;
+     cout<<f;
+     cout<<endl<<"enter 2 fraction number"<<endl;
+     cin>>f2;
+     cout<<endl<<"f2=++f"<<endl;
+     f2=++f;
+     cout<<"f  :"<<f;
+
+      cout<<"f2  :"<<f2;
+        cout<<endl<<"f2=f++"<<endl;
+        f2=f++;
+        cout<<"f  :"<<f;
+        cout<<"f2  :"<<f2;
+}*/
+/*8. Consider a class Matrix
+Class Matrix
+{
+int a[3][3];
+Public:
+//methods;
+};
+Overload the - (Unary) should negate the numbers stored in the object*/
+/*#include<iostream>
+using namespace std;
+class matrix
+{
+    int a[3][3];
+    public:
+    void inputdata();
+    void showdata();
+    void  operator-();
+    
+};
+void matrix:: operator-()
+    {
+        for(int i=0;i<3;i++)
+        {
+            for(int j=0;j<3;j++)
+            {
+                a[i][j]=-a[i][j];
+            }
+        }
+    }
+void matrix::inputdata()
+    {
+        cout<<"enter matrix(3 *3)"<<endl;
+        for(int i=0;i<3;i++)
+        {
+            for(int j=0;j<3;j++)
+            {
+                cin>>a[i][j];
+            }
+        }
+    }
+void matrix::showdata()
+{
+    cout<<"matrix is a:"<<endl;
+    for(int i=0;i<3;i++)
+        {
+            for(int j=0;j<3;j++)
+            {
+                cout<<a[i][j]<<"   ";
+            }
+            cout<<endl;
+        }
+}
+int main()
+{
+    matrix m;
+    m.inputdata();
+    m.showdata();
+    -m;
+    m.showdata();  
+}*/
+
+/*9. Consider the following class mystring
+Class mystring
+{
+char str [100];
+Public:
+// methods
+};
+Overload operator “!” to reverse the case of each alphabet in the string
+(Uppercase to Lowercase and vice versa).*/
+/*#include<string.h>
+#include<iostream>
+using namespace std;
+class mystring
+{
+    private:
+    char str[100];
+    public:
+    void setdata(char b[])
+    {
+       strcpy( str,b);
+    }
+    mystring operator!();
+    void  showdata()
+    {
+        cout<<str;
+    }
+};
+    mystring mystring::operator!()
+    {
+        mystring temp;
+        strcpy(temp.str,str);
+        for(int i=0;temp.str[i];i++)
+        {
+            if(temp.str[i]>='A' &&  temp.str[i]<='Z')
+            {
+                temp.str[i]=temp.str[i]+32;
+            }
+            if(temp.str[i]>='a' && temp.str[i]<='z')
+            {
+               temp.str[i]=temp.str[i]-32;
+            }
+        }
+        return temp;      
+    }
+int main()
+{
+    mystring s1,s2;
+    char a[]="prity";
+    s1.setdata(a);
+    s2=!s1;
+    s2.showdata();
+    s1.showdata();    
+}*/
+
+
+/*10.Class Matrix
+{
+int a[3][3];
+Public:
+//methods;
+};
+Let m1 and m2 are two matrices. Find out m3=m1+m2 (use operator
+overloading).
+Output */
+
+/*#include<iostream>
+using namespace std;
+class Matrix
+{
+    int a[3][3];
+    public:
+    void inputdata();
+    Matrix operator+(Matrix);
+    void showdata();
+};
+void  Matrix::showdata()
+{
+    
+    for(int i=0;i<3;i++)
+        {
+            for(int j=0;j<3;j++)
+            {
+                cout<<a[i][j]<<"    ";
+            }
+            cout<<endl;
+        }
+}
+Matrix Matrix:: operator+(Matrix m)
+{   
+    Matrix temp;
+    for(int i=0;i<3;i++)
+        {
+            for(int j=0;j<3;j++)
+            {
+                temp.a[i][j]=a[i][j]+m.a[i][j];
+            }
+        }
+    return temp;
+}
+    void Matrix::inputdata()
+    {
+        for(int i=0;i<3;i++)
+        {
+            for(int j=0;j<3;j++)
+            {
+             cin>>a[i][j];
+            }
+        }
+    }
+int main()
+{
+    Matrix m,m2,m3;
+    cout<<"Enter matrix one data(3*3)"<<endl;
+    cout<<"********************************"<<endl;
+    m.inputdata();
+    cout<<"first matrix is:"<<endl;
+    cout<<"********************************"<<endl;
+    m.showdata();
+    cout<<"Enter matrix two data(3*3)"<<endl;
+    cout<<"********************************"<<endl;
+    m2.inputdata();
+    cout<<"second matrix is:"<<endl;
+    cout<<"********************************"<<endl;
+    m2.showdata();
+    m3=m+m2;
+    cout<<"********************************"<<endl;
+    cout<<"addition matrix is:"<<endl;
+    m3.showdata();
+}*/
---
+++ assignment28.cpp
@@ -0,0 +1,425 @@
+/*Assignment - 28 A Job Ready Bootcamp in C++, DSA and IOT MySirG
+
+Operator Overloading, friend operator and this pointers
+
+1. Define a class Complex with appropriate instance variables and member functions.
+Overload following operators
+a. << insertion operator
+b. >> extraction operator*/
+
+/*#include<iostream>
+using namespace std;
+class complex
+{
+    private:
+        int img,real;
+    public: 
+        friend istream & operator>>(istream &,complex &);
+        friend ostream & operator<<(ostream &,complex );
+};
+ istream & operator>>(istream &in,complex &data)
+ {
+    cout<<"enter imgnary number:";
+    in>>data.img;
+    cout<<"enter real number:";
+    in>>data.real;
+    return in;
+ }
+ ostream & operator<<(ostream &out,complex data)
+ {
+    out<<"imgnary number:"<<data.img<<endl<<"real number:"<<data.real;
+    return out;
+ }
+int main()
+{
+    complex c;
+    cin>>c;
+    cout<<c;
+    return 0;
+}*/
+/*2. Define a class Complex with appropriate instance variables and member functions.
+One of the functions should be setData() to set the properties of the object. Make
+sure the names of formal arguments are the same as names of instance variables.*/
+/*#include<iostream>
+using namespace std;
+class complex{
+    private:
+        int img,real;
+    public:
+        void setdata(int img,int real)
+        {
+           (*this).img=img;
+           this->real=real;
+        }
+        void showdata()
+        {
+            cout<<"imgnary number is:"<<img<<endl<<"real number is:"<<real;
+        }
+};
+int main()
+{
+    complex c;
+    c.setdata(5,7);
+    c.showdata();
+
+}*/
+
+
+/*3. Overload subscript operator [] that will be useful when we want to check for an index
+out of bound.*/
+/*#include<iostream>
+using namespace std;
+class subscript
+{   
+    int a[8];
+   const  int size=8;
+    public:
+        subscript()
+        {       
+        }
+        int setarray(int n,int index)
+        {
+            if(index>=size)
+            {
+                exception_hendle(-1);
+            }
+            else
+            {
+                 a[index]=n;
+            }
+        }
+        void display(int index)
+        {
+             if(index>=size)
+            {
+                exception_hendle(-1);
+            }
+            else
+            {
+                 cout<<a[index]<<endl;
+            }
+        }
+        int operator[](int);
+        void  exception_hendle(int a)
+        {
+            if(a==-1)
+            {
+                cout<<"array index out of bound exption"<<endl;
+            }
+        }
+};
+int subscript:: operator[](int data)
+{
+    if(data>=size)
+    {
+        exception_hendle(-1);
+    }
+    else
+    {
+         return a[data];
+    }
+}
+int main()
+{
+    subscript s;
+    s.setarray(23,89);
+    s.display(567);
+    cout<<s[56]<<endl<<s[67];//s.operator[](4);
+    //b[20]=50;
+}*/
+
+/*4. Create a student class and overload new and delete operators as a member function
+of the class.*/
+
+/*class student
+{ 
+    int *rno;
+    char *name;
+    public:
+        operator new()
+        {
+
+        }
+
+};
+int main()
+{
+    student s;
+    new student();
+
+}*/
+
+
+
+
+/*5. Create a student class and overload new and delete operators outside the class.*/
+//6. Create a complex class and overload assignment operator for that class.
+/*#include<iostream>
+#include<string.h>
+#include<stdlib.h>
+using namespace std;
+class assignment
+{   
+    public:
+    char *a;
+    int b;
+    public:
+    assignment(const char p[],int c)
+    {
+        a=(char *)calloc(1,sizeof(char));
+        strcpy(a,p);
+        b=c;
+    }
+    void update(const char data[])
+    {
+        strcpy(a,data);
+    }
+    void operator=(assignment &c)
+    {
+        c.a=(char *)calloc(1,sizeof(char));
+       strcpy(c.a,a);
+        c.b=b;
+    }
+    void showdata()
+    {
+        cout<<a<<" "<<b<<endl;
+    }
+    assignment(){}
+};
+int main()
+{
+    assignment A("prity",23),B;
+    cout<<"object A data:";
+    A.showdata();
+   A=B;
+    A.update("kyada");
+    cout<<endl<<"object A data:";
+    A.showdata();
+    cout<<endl<<"object B data:";
+    B.showdata();
+    return 0;
+}*/
+/*#include<iostream>
+#include<string.h>
+using namespace std;
+#include<cstring>
+
+class mystring
+{
+    private:
+        char *p;
+        int size;
+    public:
+    mystring(char *a  )
+    {
+        cout<<"parameterized constructor call"<<endl;
+        size=strlen(a);
+        p=(char *)malloc(size+1);
+        strcpy(p,a);
+    }
+    mystring(mystring &a)
+    {
+        cout<<"copy constructor call"<<endl;
+        p=(char *)malloc(a.size+1);
+        strcpy(p,a.p);
+    }
+    void changevalue(char * d)
+    {
+        size=strlen(d);
+        p=(char *)realloc(p,size+1);
+        strcpy(p,d);
+    }
+    void showdata()
+    {
+        cout<<"value is a:"<<p<<endl;
+    }
+    mystring()
+    {
+        cout<<"default constructor call"<<endl;
+        p=(char *)malloc(1);
+    
+    }
+   mystring & operator=( mystring &data)
+   {
+    cout<<"assignment operator call"<<endl;
+    if(p==data.p)
+    {
+        return  *this;
+    }
+        p=(char*)realloc(p,data.size+1);
+       // p=(char *)malloc(data.size+1);
+        strcpy(p,data.p);
+   }
+};
+int main()
+{
+    mystring m("prity");
+    mystring n=m;
+    m.changevalue("kyada");
+    m.showdata();
+    n.showdata();
+    mystring o("xyzw");
+    o=m;
+    mystring q;
+    q=m;//p erase so create assign 1 block in default constructor
+    m.changevalue("bali");
+    m.showdata();
+    o.showdata();
+    q.showdata();
+    m=m;
+    cout<<"\n";
+    mystring r;
+    m.showdata();
+    r=q=m;
+    cout<<"\n";
+    r.showdata();
+    return 0;
+}*/
+
+//7. Create an Integer class and overload logical not operator for that class.
+/*#include<iostream>
+using namespace std;
+class integer
+{
+    int num;
+    public:
+    void setdata(int v)
+    {
+        num=v;
+    }
+    integer operator~()
+    {
+        integer temp;
+        temp.num=~num;
+        return temp;
+    }
+    void showdata()
+    {
+        cout<<endl<<"value is a:"<<num;
+    }
+};
+int main()
+{
+    integer i,i2;
+    i.setdata(2);
+    i2=~i;
+    i.showdata();
+    i2.showdata();
+    return 0;
+}*/
+
+
+/*8. Create a Coordinate class for 3 variables x,y and z and overload comma operator
+such that when you write c3 = (c1 , c2 ) then c2 is assigned to c3. Where c1,c2,and
+c3 are objects of 3D coordinate class.*/
+/*#include<iostream>
+using namespace std;
+class coordinate
+{
+    private:
+        int x,y,z;
+    public:
+        coordinate(int a,int b,int c)
+        {
+            x=a;
+            y=b;
+            z=c;
+        }
+        coordinate(){}
+        void showdata()
+        {
+            cout<<"x-->"<<x<<"y-->"<<y<<"z-->"<<z<<endl;
+        }
+};
+int main()
+{
+    coordinate c1(4,5,6),c2(5,7,8),c3(5,8,9);
+    //coordinate c3=(c1,c2);
+    c3=(c1,c2);
+    c1.showdata();
+    c2.showdata();
+    c3.showdata();
+    return 0;
+}*/
+
+/*9. Create an Integer class that contains int x as an instance variable and overload
+casting int() operator that will type cast your Integer class object to int data type.
+
+
+10. Create a Distance class having 2 instance variable feet and inches. Also create
+default constructor and parameterized constructor takes 2 variables . Now overload ()
+function call operator that takes 3 arguments a , b and c and set feet = a + c + 5 and
+inches = a+b + 15.*/
+/*#include<iostream>
+using namespace std;
+class distance1
+{
+    private:
+    float feet,inches;
+    public:
+    distance1(){cout<<"default constructor call"<<endl;};
+    distance1(int a,int b)
+    {
+        cout<<"parameterized constructor call"<<endl;
+        feet=a;
+        inches=b;
+    }
+    distance1  operator()(int a,int b,int c)
+    {
+        distance1 d;
+        d.feet=a+c+5;
+        d.inches=a+b+15;
+        return d;
+    }
+    distance1(distance1 &t)
+    {
+        cout<<"copy constructor call"<<endl;
+        feet=t.feet;
+        inches=t.inches;
+    }
+    void showdata()
+    {
+        cout<<"feet-->"<<feet<<"inches-->"<<inches<<endl;
+    }
+    friend ostream & operator<<(ostream &,distance1 );
+};
+   ostream & operator<<(ostream &in,distance1 data)
+   {
+        in<<"feet-->"<<data.feet<<"inches-->"<<data.inches<<endl;
+   }  
+int main()
+{
+    distance1 d(4,6);
+    distance1 d2;
+    d2=d(5,7,7);
+    cout<<d2;
+    d.showdata();
+    d2.showdata();
+}*/
+
+/*11. Create a class Marks that have one member variable marks and one member
+function that will print marks. We know that we can access member functions using
+(.) dot operator. Now you need to overload (->) arrow operator to access that
+function.*/
+/*#include<iostream>
+using namespace std;
+class mark
+{
+    private:
+    int marks;
+    public:
+        mark(int data)
+        {
+            marks=data;
+        }
+        void showdata()
+        {
+            cout<<"marks is a:"<<marks;
+        }
+};
+int main()
+{
+    mark m(56);
+    mark *p=&m;
+    p->showdata();
+}*/
